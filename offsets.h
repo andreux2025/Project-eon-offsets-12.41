0062f39517d943decc84c6695b627174ecddc766
andreux2025 <turandreu124@gmail.com>
2025-04-05 02:48:52 -0700

    Create offsets.h

---
+++ offsets.h
@@ -0,0 +1,9 @@
+namespace Offsets
+{
+	constexpr int32 GObjects          = 0x08067DF0;
+	constexpr int32 AppendString      = 0x02C1DEA0;
+	constexpr int32 GNames            = 0x0804F780;
+	constexpr int32 GWorld            = 0x08155D80;
+	constexpr int32 ProcessEvent      = 0x02E13BF0;
+	constexpr int32 ProcessEventIdx   = 0x00000042;
+}
